0d1f23dc2a978f7becfe10e85fd49668db868c09
MERCYGATUNE <ariri691@gmail.com>
2024-05-05 23:23:38 +0300

    completed

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,16 +3,16 @@ import {useState,useEffect} from 'react'
 
 const url ='http://localhost:3000/bots'
 function Search() {
-    const [query,setQuery]= useState('name');
+    const [query,setQuery]= useState('');
     // SYNTAX FOR USEEFFECT
     useEffect(()=> {
-       async function fetchBot(){
-            console.log('here')
-          const res= await fetch(`${url} ? query=${query}`)
+      async function fetchBot(){
+            
+          const res= await fetch(`${url} `)
           const data= await res.json ()
-          console.log(data.results)
+          console.log(data)
         }
-        fetchBot()
+         fetchBot()
     },[query],)
   return (
     <div>
